fd328f9a27ca304ea3b42030128f0f5e6fd51d96
Artem Demo <artemdemo@gmail.com>
2019-12-11 23:48:30 +0200

    reorganized "Add New" link

--- wp-content/plugins/rough-chart/views/templates/admin-tmpl.php
+++ wp-content/plugins/rough-chart/views/templates/admin-tmpl.php
@@ -1,14 +1,20 @@
 <?php
-    $link_args = array();
-    $link_args[RoughChartAdmin::$chart_id_arg] = 'new';
-    $add_new_link = add_query_arg(
-	    $link_args,
-        menu_page_url( 'rough_chart', false )
-    )
+    function rough_chart_get_url_to_add_new() {
+	    $link_args = array();
+	    $link_args[RoughChartAdmin::$chart_id_arg] = 'new';
+        return add_query_arg(
+	        $link_args,
+	        menu_page_url( 'rough_chart', false )
+        );
+    }
+
 ?>
 <div class="wrap">
     <h1 class="wp-heading-inline">Rough Charts</h1>
-    <a href="<?= $add_new_link ?>" class="page-title-action">Add New</a>
+    <a href="<?= rough_chart_get_url_to_add_new() ?>"
+       class="page-title-action">
+        Add New
+    </a>
     <hr class="wp-header-end">
     <h2 class="screen-reader-text">Rough Charts list</h2>
 </div>
