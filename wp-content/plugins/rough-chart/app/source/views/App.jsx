d9452e87193fa942514f4c70571283268b6632fd
Artem Demo <artemdemo@gmail.com>
2019-12-17 22:04:05 +0200

    added data table

--- wp-content/plugins/rough-chart/app/source/views/App.jsx
+++ wp-content/plugins/rough-chart/app/source/views/App.jsx
@@ -8,9 +8,30 @@ class App extends Component {
     render(props, state) {
         return (
             <Fragment>
-                <h1 className="wp-heading-inline">New Rough Chart</h1>
-                <hr className="wp-header-end" />
-                <h2 className="screen-reader-text">New Rough Chart options</h2>
+                <h1 className='wp-heading-inline'>New Rough Chart</h1>
+                <hr className='wp-header-end' />
+                <h2 className='screen-reader-text'>New Rough Chart options</h2>
+                <p>Define your new chart by defining options below:</p>
+                <table className='form-table' role='presentation'>
+                    <tbody>
+                        <tr className='form-field form-required'>
+                            <th scope='row'>
+                                <label htmlFor='chart_title'>
+                                    Title
+                                </label>
+                            </th>
+                            <td>
+                                <input
+                                    id='chart_title'
+                                    type='text'
+                                    aria-required='true'
+                                    autoCapitalize='none'
+                                    autoCorrect='off'
+                                />
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
             </Fragment>
         )
     }
