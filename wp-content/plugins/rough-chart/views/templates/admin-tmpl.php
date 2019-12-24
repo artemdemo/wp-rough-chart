8bde31dcbead7e9fccd0ec0c5ed16ca10a399ea6
Artem Demo <artemdemo@gmail.com>
2019-12-24 22:22:14 +0200

    reorganized table

--- wp-content/plugins/rough-chart/views/templates/admin-tmpl.php
+++ wp-content/plugins/rough-chart/views/templates/admin-tmpl.php
@@ -11,33 +11,37 @@ use roughChart\views\AdminView;
     </a>
     <hr class="wp-header-end">
     <h2 class="screen-reader-text">Rough Charts list</h2>
-    <? if ( count( AdminView::get_charts() ) > 0 ): ?>
-        <table class="wp-list-table widefat fixed striped">
-            <thead>
-                <tr>
-                    <th scope="col" class="manage-column column-name column-primary">
-                        <span>title</span>
-                    </th>
-                    <th scope="col" class="manage-column column-name column-primary">
-                        <span>created</span>
-                    </th>
-                    <th scope="col" class="manage-column column-name column-primary">
-                        <span>last updated</span>
-                    </th>
-                </tr>
-            </thead>
-            <tbody>
-            <? foreach( AdminView::get_charts() as $chart ): ?>
+    <table class="wp-list-table widefat fixed striped">
+        <thead>
+        <tr>
+            <th scope="col" class="manage-column column-name column-primary">
+                <span>Title</span>
+            </th>
+            <th scope="col" class="manage-column column-name column-primary">
+                <span>Created</span>
+            </th>
+            <th scope="col" class="manage-column column-name column-primary">
+                <span>Last updated</span>
+            </th>
+        </tr>
+        </thead>
+        <tbody>
+        <? if ( count( AdminView::get_charts() ) > 0 ): ?>
+	        <? foreach( AdminView::get_charts() as $chart ): ?>
                 <tr>
                     <th><?= $chart -> title ?></th>
                     <td><?= $chart -> created ?></td>
                     <td><?= $chart -> last_updated ?></td>
                 </tr>
-            <? endforeach; ?>
-            </tbody>
-        </table>
-    <? else: ?>
-        No charts
-    <? endif; ?>
+	        <? endforeach; ?>
+        <? else: ?>
+            <tr>
+                <td colspan="3">
+                    No charts
+                </td>
+            </tr>
+        <? endif; ?>
+        </tbody>
+    </table>
 
 </div>
