522a153f5e830d8a1d3a5108aad675549c64cdc6
Artem Demo <artemdemo@gmail.com>
2020-01-14 23:08:13 +0200

    added state to ColumnFields

--- wp-content/plugins/rough-chart/app/source/containers/ChartFields/ColumnsFields.tsx
+++ wp-content/plugins/rough-chart/app/source/containers/ChartFields/ColumnsFields.tsx
@@ -1,13 +1,48 @@
 import React from 'react';
+import ChartData from '../ChartData/ChartData';
+import { defaultStyle } from '../formProps/FillStyle';
+import { defaultLegend } from '../formProps/Legend';
 
 interface IProps {
     chartProps: any
     chartId?: number|string;
     disabled?: boolean;
 }
-interface IState {}
+interface IState {
+    title: string;
+    fillStyle: string;
+    strokeWidth: string;
+    strokeWidthErr: boolean;
+    fillWeight: string;
+    fillWeightErr: boolean;
+    roughness: string;
+    roughnessErr: boolean;
+    legend: string;
+    dataUpdated: boolean;
+}
 
 class ColumnsFields extends React.PureComponent<IProps, IState> {
+    private chartDataRef = React.createRef<ChartData>();
+
+    static defaultPros = {
+        chartProps: null,
+        chartId: undefined,
+        disabled: false,
+    };
+
+    public state = {
+        title: '',  // title can be empty
+        fillStyle: defaultStyle.type,
+        legend: defaultLegend.type,
+        strokeWidth: '',
+        strokeWidthErr: false,
+        fillWeight: '',
+        fillWeightErr: false,
+        roughness: '',
+        roughnessErr: false,
+        dataUpdated: false,
+    };
+
     render() {
         return (
             <React.Fragment>
