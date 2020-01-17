217618479f7b5e9e5be1337c63a84d5fbe482c34
Artem Demo <artemdemo@gmail.com>
2020-01-17 12:53:39 +0200

    display different charts from roughViz

--- wp-content/plugins/rough-chart/app/source/shortcode.ts
+++ wp-content/plugins/rough-chart/app/source/shortcode.ts
@@ -1,27 +1,66 @@
 import roughViz from 'rough-viz';
 import { parseChart } from './shortcode/chartData';
-import { TChartShortcode } from './chartTypes';
+import { TChartShortcode, TChartTypes } from './chartTypes';
 import { ELegendTypes } from './containers/formProps/Legend';
+
+type TRoughVizSettings = {
+    element: string;
+    title: string;
+    fillWeight: number;
+    roughness: number;
+    strokeWidth: number;
+    legend: boolean;
+    legendPosition: string;
+    data: {
+        labels: string[];
+        values: string[]|number[];
+    };
+    color?: string;
+    colors?: string[];
+    fillStyle?: string;
+};
 
 const __addRoughChart = (chartInput: TChartShortcode) => {
     const chartOptions = parseChart(chartInput.chart);
+
     if (chartOptions && chartOptions.data) {
         const legendTypeNum = parseInt(chartOptions.legend, 10);
-        new roughViz.Pie({
+        const roughVizSettings: TRoughVizSettings = {
             element: '.' + chartInput.className,
             title: chartInput.title,
-            fillStyle: chartOptions.fillStyle,
             fillWeight: chartOptions.fillWeight,
             roughness: chartOptions.roughness,
             strokeWidth: chartOptions.strokeWidth,
             legend: legendTypeNum !== ELegendTypes.hidden,
             legendPosition: legendTypeNum === ELegendTypes.left ? 'left' : 'right',
-            colors: chartOptions.data.colors,
             data: {
                 labels: chartOptions.data.labels,
                 values: chartOptions.data.values,
             },
-        });
+        };
+
+        if (TChartTypes[chartInput.chart_type] === TChartTypes.pie) {
+            roughVizSettings.fillStyle = chartOptions.fillStyle;
+            roughVizSettings.colors = chartOptions.data.colors;
+            new roughViz.Pie(roughVizSettings);
+        }
+
+        if (TChartTypes[chartInput.chart_type] === TChartTypes.bars) {
+            roughVizSettings.fillStyle = chartOptions.fillStyle;
+            roughVizSettings.color = chartOptions.color;
+            new roughViz.BarH(roughVizSettings);
+        }
+
+        if (TChartTypes[chartInput.chart_type] === TChartTypes.columns) {
+            roughVizSettings.fillStyle = chartOptions.fillStyle;
+            roughVizSettings.color = chartOptions.color;
+            new roughViz.Bar(roughVizSettings);
+        }
+
+        if (TChartTypes[chartInput.chart_type] === TChartTypes.lines) {
+            roughVizSettings.color = chartOptions.color;
+            new roughViz.Line(roughVizSettings);
+        }
     }
 };
 
